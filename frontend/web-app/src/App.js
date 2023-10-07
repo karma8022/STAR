e64f66dca7e544de086a6ad4ebcf04dd652b36f3
Saubhagya Singh <76896543+SaubhagyaSingh@users.noreply.github.com>
2023-10-08 00:41:19 +0530

    signup page

--- frontend/web-app/src/App.js
+++ frontend/web-app/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { Navbar } from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 // import SignIn from "./pages/Signin.jsx";
-// import SignUp from "./pages/Signup.jsx";
+import SignUp from "./pages/Signup.jsx";
 import Profile from "./pages/Profile.js";
 import Docs from "./pages/Docs";
 import Projects from "./pages/Projects";
@@ -11,18 +11,18 @@ import Home from "./pages/Home.js";
 import "./style.css";
 function App() {
   return (
-		<div className="bg-base-100">
-			<Navbar />
-			<Routes>
-				{/* <Route path="/signup" element={<SignUp />} /> */}
-				{/* <Route path="/signin" element={<SignIn />} /> */}
-				<Route path="/profile" element={<Profile />} />
-				<Route path="/docs" element={<Docs />} />
-				<Route path="/" element={<Home />} />
-				<Route path="/projects" element={<Projects />} />
-				<Route path="/about" element={<About />} />
-			</Routes>
-		</div>
+    <div className="bg-base-100">
+      <Navbar />
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        {/* <Route path="/signin" element={<SignIn />} /> */}
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/docs" element={<Docs />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
   );
 }
 
