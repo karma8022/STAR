128b3fcf5d3a5e89c9c664284bafd568bc6008d1
Sahaj Mishra <sahajm2027@gmail.com>
2023-10-08 16:02:02 +0530

    Corrected everything in backend

--- backend/query/urls.py
+++ backend/query/urls.py
@@ -9,4 +9,6 @@ urlpatterns=[
     path('nasa/', views.nasa, name='nasa'),
     path('bulletin/', views.bulletin, name='bulletin'),
     path('pre_queries/', views.queries_request, name='queries_request'),
+    path('spacetech/', views.spacetech, name='spacetech'),
+    path('standards/', views.standards, name='standards')
 ]
